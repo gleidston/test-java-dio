0738843f4acc0319db43acb8ea1e54e750f4848e
Gleidston <king2010-ro@hotmail.com>
2021-12-26 21:44:29 -0300

    Iniciando classe para  testes e debug

---
+++ src/br/com/ola/ola.java
@@ -0,0 +1,13 @@
+package br.com.ola;
+
+import br.com.ola.model.Gato;
+
+public class ola {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println("ola seja bem vindo ao sistema :");
+        System.out.println(gato);
+
+    }
+}
